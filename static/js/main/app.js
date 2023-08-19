c7be269c3194bc4db9e64997cb8f200f794adbce
Sunggyu Han <gkstjdrb00@soongsil.ac.kr>
2023-08-19 14:02:51 +0900

    main 수정

--- static/js/main/app.js
+++ static/js/main/app.js
@@ -16,3 +16,21 @@ genreItems.forEach(item => {
     console.log('선택한 장르:', selectedGenre);
   });
 });
+
+const searchBtn = document.getElementById('search-btn');
+searchBtn.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const searchInput = document.getElementById('search-input');
+  const searchTerm = searchInput.value.trim().toLowerCase();
+
+
+  const searchMessage = document.getElementById('search-message');
+  if (searchTerm) {
+    searchMessage.textContent = `"${searchTerm}" 작품을 찾으시나요?`;
+    searchMessage.classList.remove('hidden');
+  } else {
+    searchMessage.classList.add('hidden');
+  }
+
+});
